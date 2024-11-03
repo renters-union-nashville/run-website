34dda87fef62e9ad49942607115a34c854188927
Greg Ziegan <greg.ziegan@gmail.com>
2024-11-03 15:02:42 -0600

    green

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './components/theme-provider'
 import { ModeToggle } from './components/mode-toggle'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
