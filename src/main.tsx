2f3ce06564239d2190b0e495b083aecc04bd0409
Greg Ziegan <greg.ziegan@gmail.com>
2024-11-08 15:41:45 -0600

    try hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter
+    <HashRouter
       future={{
         v7_startTransition: true,
         v7_relativeSplatPath: true,
       }}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
